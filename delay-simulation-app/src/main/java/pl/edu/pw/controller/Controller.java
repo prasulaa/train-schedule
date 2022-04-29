7253229a428ec1df1c9f2058efe26aca295f87f8
lilartisto <orginalartis@gmail.com>
2022-04-29 21:01:50 +0200

    added sending PUT request to api from delay-simulation-app to change delay

--- delay-simulation-app/src/main/java/pl/edu/pw/controller/Controller.java
+++ delay-simulation-app/src/main/java/pl/edu/pw/controller/Controller.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Controller {
 
     @FXML
@@ -26,7 +30,24 @@ public class Controller {
 
     @FXML
     private void setDelayButtonClicked() {
-        
+        if (setDelay(trainScheduleIdSpinner.getValue(), delaySpinner.getValue())) {
+            setDelayButton.setStyle("-fx-border-color: #00ff00");
+        } else {
+            setDelayButton.setStyle("-fx-border-color: #ff0000");
+        }
+    }
+
+    private boolean setDelay(Integer trainScheduleId, Integer delay) {
+        try {
+            URL url = new URL("http://localhost:8080/trainschedule/" + trainScheduleId + "?delay=" + delay);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PUT");
+            int responseCode = connection.getResponseCode();
+            return responseCode == 204;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
     }
 
 }
